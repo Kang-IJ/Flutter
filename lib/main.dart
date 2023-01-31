fe9252cde90572251ee9e9c04bb9af7d03a70a52
Kang-IJ <rkddl6803@naver.com>
2023-01-31 23:52:51 +0900

    feat: add interactive buttons

--- lib/main.dart
+++ lib/main.dart
@@ -6,21 +6,91 @@ void main() {
 }
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(num) async {
+    final player = AudioPlayer();
+    await player.play(
+      AssetSource('note$num.wav'),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () async {
-                final player = AudioPlayer();
-                await player.play(
-                  AssetSource('note1.wav'),
-                );
-              },
-              child: Text('Click Me!'),
-            ),
+          child: Column(
+            children: [
+              TextButton(
+                onPressed: () {
+                  playSound('1');
+                },
+                child: Container(
+                  width: 150.0,
+                  height: 50.0,
+                  color: Colors.red,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  playSound('2');
+                },
+                child: Container(
+                  width: 150.0,
+                  height: 50.0,
+                  color: Colors.orange,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  playSound('3');
+                },
+                child: Container(
+                  width: 150.0,
+                  height: 50.0,
+                  color: Colors.yellow,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  playSound('4');
+                },
+                child: Container(
+                  width: 150.0,
+                  height: 50.0,
+                  color: Colors.green,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  playSound('5');
+                },
+                child: Container(
+                  width: 150.0,
+                  height: 50.0,
+                  color: Colors.teal,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  playSound('6');
+                },
+                child: Container(
+                  width: 150.0,
+                  height: 50.0,
+                  color: Colors.indigo,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  playSound('7');
+                },
+                child: Container(
+                  width: 150.0,
+                  height: 50.0,
+                  color: Colors.purple,
+                ),
+              ),
+            ],
           ),
         ),
       ),
