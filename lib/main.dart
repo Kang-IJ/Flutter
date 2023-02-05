533965fc4689bfecdb3fe5dabe17c6cf8e3c42e6
Kang-IJ <rkddl6803@naver.com>
2023-02-05 21:00:27 +0900

    feat: remove all unnecessary todos

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'story_brain.dart';
 
-//TODO: Step 15 - Run the app and see if you can see the screen update with the first story. Delete this TODO if it looks as you expected.
-
 void main() {
   runApp(const Destini());
 }
@@ -17,8 +15,6 @@ class Destini extends StatelessWidget {
     );
   }
 }
-
-//TODO: Step 9 - Create a new storyBrain object from the StoryBrain class.
 
 class StoryPage extends StatefulWidget {
   const StoryPage({Key? key}) : super(key: key);
@@ -64,13 +60,11 @@ class _StoryPageState extends State<StoryPage> {
                   ),
                   onPressed: () {
                     //Choice 1 made by user.
-                    //TODO: Step 18 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user.
                     setState(() {
                       storyBrain.nextStory(1);
                     });
                   },
                   child: Text(
-                    //TODO: Step 13 - Use the storyBrain to get the text for choice 1.
                     storyBrain.getChoice1(storyBrain.storyNumber),
                     style: TextStyle(
                       fontSize: 20.0,
@@ -84,8 +78,6 @@ class _StoryPageState extends State<StoryPage> {
               ),
               Expanded(
                 flex: 2,
-                //TODO: Step 26 - Use a Flutter Visibility Widget to wrap this FlatButton.
-                //TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
                 child: Visibility(
                   visible: storyBrain.buttonShouldBeVisible(),
                   child: TextButton(
@@ -94,14 +86,12 @@ class _StoryPageState extends State<StoryPage> {
                     ),
                     onPressed: () {
                       //Choice 2 made by user.
-                      //TODO: Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
                       setState(() {
                         storyBrain.nextStory(2);
                       });
                     },
                     child: Text(
                       storyBrain.getChoice2(storyBrain.storyNumber),
-                      //TODO: Step 14 - Use the storyBrain to get the text for choice 2.
                       style: TextStyle(
                         fontSize: 20.0,
                         color: Colors.black,
@@ -117,7 +107,3 @@ class _StoryPageState extends State<StoryPage> {
     );
   }
 }
-
-//TODO: Step 24 - Run the app and try to figure out what code you need to add to this file to make the story change when you press on the choice buttons.
-
-//TODO: Step 29 - Run the app and test it against the Story Outline to make sure you've completed all the steps. The code for the completed app can be found here: https://github.com/londonappbrewery/destini-challenge-completed/
