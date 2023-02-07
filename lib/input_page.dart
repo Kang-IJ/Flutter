f2d308831185e125822cd30fa66ebf45f9c09ed4
Kang-IJ <rkddl6803@naver.com>
2023-02-07 23:05:15 +0900

    feat: refactor using enums and ternary operator

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -5,7 +5,14 @@ import 'reusable_card.dart';
 
 const bottomContainerHeight = 80.0;
 const activeCardColor = Color(0xFF1D1E33);
+const inactiveCardColor = Color(0xFF111328);
 const bottomContainerColor = Color(0xFFEB1555);
+
+enum Gender {
+  male,
+  female,
+  neither,
+}
 
 class InputPage extends StatefulWidget {
   @override
@@ -13,6 +20,8 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  Gender selectedGender = Gender.neither;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,15 +34,33 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: [
                 Expanded(
-                  child: ReusableCard(
-                    activeCardColor,
-                    IconContent(FontAwesomeIcons.mars, 'MALE'),
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectedGender = Gender.male;
+                      });
+                    },
+                    child: ReusableCard(
+                      selectedGender == Gender.male
+                          ? activeCardColor
+                          : inactiveCardColor,
+                      IconContent(FontAwesomeIcons.mars, 'MALE'),
+                    ),
                   ),
                 ),
                 Expanded(
-                  child: ReusableCard(
-                    activeCardColor,
-                    IconContent(FontAwesomeIcons.venus, 'FEMALE'),
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectedGender = Gender.female;
+                      });
+                    },
+                    child: ReusableCard(
+                      selectedGender == Gender.female
+                          ? activeCardColor
+                          : inactiveCardColor,
+                      IconContent(FontAwesomeIcons.venus, 'FEMALE'),
+                    ),
                   ),
                 ),
               ],
