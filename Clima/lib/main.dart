57b9b59e54cdf0c9f3385dc40a07a72c8eece3c0
Kang-IJ <rkddl6803@naver.com>
2023-02-15 16:03:09 +0900

    feat: finish by adding input functionality and editing styles such as fonts, etc.

--- Clima/lib/main.dart
+++ Clima/lib/main.dart
@@ -4,10 +4,16 @@ import 'package:clima/screens/loading_screen.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final ThemeData theme = ThemeData();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark(),
+      theme: ThemeData.dark().copyWith(
+        colorScheme: theme.colorScheme.copyWith(
+          primary: Colors.white,
+        ),
+      ),
       home: LoadingScreen(),
     );
   }
--- Clima/lib/screens/loading_screen.dart
+++ Clima/lib/screens/loading_screen.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import '/services/location.dart';
-import 'package:clima/services/networking.dart';
-import 'package:clima/utilities/apikey.dart';
 import 'location_screen.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:clima/services/weather.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -18,14 +16,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getLocationData() async {
-    Location location = Location();
-    APIKey api = APIKey();
-    await location.getCurrentLocation();
-
-    NetworkHelper networkHelper = NetworkHelper(
-        'https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=${api.apiKey}&units=metric');
-
-    var weatherData = await networkHelper.getData();
+    var weatherData = await WeatherModel().getLocationWeather();
 
     Navigator.push(context, MaterialPageRoute(builder: (context) {
       return LocationScreen(
