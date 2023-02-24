bb07fa6297efb35e48d2ec40a135e52e4a824bb1
Kang-IJ <rkddl6803@naver.com>
2023-02-24 18:09:30 +0900

    feat: refactor input decoration

--- flash_chat/lib/components/rounded_button.dart
+++ flash_chat/lib/components/rounded_button.dart
@@ -25,6 +25,9 @@ class RoundedButton extends StatelessWidget {
           height: 42.0,
           child: Text(
             buttonText,
+            style: TextStyle(
+              color: Colors.white,
+            ),
           ),
         ),
       ),
